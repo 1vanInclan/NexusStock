e77b3393d2d52ceaee998b70d9f4a47c51c481a3
Ivan <ivan.inclan.327b@gmail.com>
2026-04-17 17:42:08 -0600

    Agregue swagger

--- src/inventory/inventory.controller.ts
+++ src/inventory/inventory.controller.ts
@@ -1,7 +1,9 @@
 import { Controller, Get, Post, Body, Param } from '@nestjs/common';
 import { InventoryService } from './inventory.service';
 import { CreateInventoryDto } from './dto/create-inventory.dto';
+import { ApiTags } from '@nestjs/swagger';
 
+@ApiTags('inventory')
 @Controller('inventory')
 export class InventoryController {
   constructor(private readonly inventoryService: InventoryService) {}
--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { HttpAdapterHost, NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { PrismaClientExceptionFilter } from './common/filters/prisma-client-exception.filter';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -16,6 +17,21 @@ async function bootstrap() {
       transform: true
     })
   )
+
+  // Configuración de Swagger
+  const config = new DocumentBuilder()
+    .setTitle('Nexus Stock API')
+    .setDescription('Sistema de gestión de inventarios y control de stock')
+    .setVersion('1.0')
+    .addTag('products')
+    .addTag('categories')
+    .addTag('inventory')
+    .build();
+  
+  const document = SwaggerModule.createDocument(app, config)
+
+  SwaggerModule.setup('api', app, document)
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
